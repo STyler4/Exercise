8f29e893eb0eaf45d45f46f847714ece17705b10
STyler4 <98915469+STyler4@users.noreply.github.com>
2022-02-10 09:35:29 +1300

    Drivers_licence

---
+++ balance.py
@@ -0,0 +1,7 @@
+Too_Young = 15
+age = int(input('what is your age? '))
+
+if age > Too_Young:
+    print("Can apply for Learner Licence")
+if age < Too_Young:
+    print("Unable to apply for Learner Licence")
