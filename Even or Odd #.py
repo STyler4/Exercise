b6d70de636e97e5027f0e9a4453db36afd1060e4
STyler4 <98915469+STyler4@users.noreply.github.com>
2022-02-14 12:23:12 +1300

    Even or Odd Calculator

---
+++ Even or Odd #.py
@@ -0,0 +1,5 @@
+numbers = int(input("Please enter a number: "))
+if numbers % 2 == 0:
+    print(f"{numbers} is even")
+else:
+    print(f"{numbers} is odd")
--- Person.py
+++ Person.py
@@ -1,7 +1,9 @@
 Age = float(input("Enter your age: "))
 Weight = float(input("Enter your weight: "))
+MIN_AGE = 16
+MIN_WEIGHT = 50
 
-if Age < 16 or Weight < 50:
+if Age < MIN_AGE or Weight < MIN_WEIGHT:
     print("You can't give blood")
 
 else:
