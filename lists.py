47732f63d9c1e5ac19c214855764190748de738b
STyler4 <98915469+STyler4@users.noreply.github.com>
2022-02-22 15:08:26 +1300

    lists

---
+++ lists.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 30, 40]
+fruit = ["pineapple", "apple", "banana", "orange", "cherry", "kiwifruit", "melon", "mango"]
+
+print(numbers[len(numbers)-1])
+# print(fruit[-2])
+# print(fruit[-1])
+
